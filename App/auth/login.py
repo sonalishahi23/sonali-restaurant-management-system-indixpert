6297603013ee266d38df6e772639f6c002e6e45c
sonalishahi23 <sonalirajput160@gmail.com>
2026-04-01 00:21:38 +0530

    Login Module

--- App/auth/login.py
+++ App/auth/login.py
@@ -2,9 +2,24 @@ import json
 from validation.auth_validation import EmailValidation, PasswordValidation
 from admin_management.admin_dashboard import Admin_dashboard
 from staff_management.staff_dashboard import StaffDashboard
+from datetime import datetime
+import os
 
+
+os.makedirs("App/logs", exist_ok=True)
 class Login:
+             
+    def write_login_log(self, email, role, success=True):
+        """Append login attempt info to a log file"""
+        now = datetime.now()
+        date_time = now.strftime("%d-%m-%Y %H:%M:%S")
+        log_file = "App/logs/login_logs.txt"
 
+        status = "Successful" if success else "Failed"
+
+        with open(log_file, "a") as file:  # no UTF needed
+            file.write(f"{date_time} | Email: {email} | Role: {role} | Login {status}\n")
+          
     def login_user(self):
 
         email_validator = EmailValidation()
@@ -46,6 +61,8 @@ class Login:
                     print("\n" + "-" * 60)
                     print(f"Login Successful! Welcome, {user['name']} ")
                     print("-" * 60)
+
+                    self.write_login_log(email, user["role"], success=True)
             
                     if user["role"] == "admin":
                         print("Redirecting to Admin Dashboard...\n")
@@ -62,5 +79,7 @@ class Login:
 
         if not found_email:
             print("Email not registered. Please sign up first.")
+            self.write_login_log(email, "Unknown", success=False)
         elif not password_correct:
             print("Invalid Email or Password")
+            self.write_login_log(email, "Unknown", success=False)
