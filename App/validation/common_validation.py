27429faef0212caedb79dd1cb33e39ef6635167a
sonalishahi23 <sonalirajput160@gmail.com>
2026-03-31 20:53:46 +0530

    validations

--- App/validation/common_validation.py
+++ App/validation/common_validation.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 class CommonValidation:
 
     def validate_choice(self, choice, min_val, max_val):
@@ -35,7 +36,7 @@ class CommonValidation:
     
     def validate_item_id(self, item_id):
         if not item_id.strip():
-            print("❌ Item ID cannot be empty")
+            print("Item ID cannot be empty")
             return None
 
         item_id = item_id.strip().upper()
@@ -88,3 +89,50 @@ class CommonValidation:
         except:
             print("Invalid salary! Enter numbers only.")
             return None
+        
+    def validate_date(self, date_str):
+        try:
+            valid_date = datetime.strptime(date_str, "%d-%m-%Y").date()
+            today = datetime.today().date()
+            max_booking = today + timedelta(days=7)
+            
+            if valid_date < today or valid_date > max_booking:
+                print(f"Invalid date! You can book only within 7 days from today.")
+                return None
+            
+            return valid_date  
+        except ValueError:
+            print("Invalid date format! Please enter in dd-mm-yyyy format.")
+            return None
+        
+    def validate_seats(self, seats_str, tables):
+        """Validate required seats."""
+        if not seats_str.isdigit():
+           print("Seats must be a numeric value.")
+           return None
+
+        seats = int(seats_str)
+        if seats < 1:
+            print("You must book at least 1 seat.")
+            return None
+
+    
+        if not any(table["seats"] >= seats for table in tables):
+            print("No table available for this many seats.")
+            return None
+
+        return seats
+    
+    def validate_booking_id(self, booking_id):
+        """Validate booking ID: numeric and 8 digits."""
+        booking_id = str(booking_id)  # ensure it's string for isdigit()
+        
+        if not booking_id.isdigit():
+            print("Booking ID must be numeric.")
+            return None
+
+        if len(booking_id) != 8:
+            print("Booking ID must be 8 digits.")
+            return None
+
+        return int(booking_id)
