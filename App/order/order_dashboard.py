be784c6178f60f9a91d03e9bc2ea371dadafdbc5
sonalishahi23 <sonalirajput160@gmail.com>
2026-03-31 14:06:24 +0530

    Order Management

---
+++ App/order/order_dashboard.py
@@ -0,0 +1,130 @@
+from order.order_operation import OrderOperations
+from validation.common_validation import CommonValidation
+from menu.view_menu import ViewMenu
+
+class OrderDashboard:
+
+    def __init__(self):
+        self.order = OrderOperations()
+        self.validator = CommonValidation()
+
+    def menu(self):
+        while True:
+            print("\n" + "=" * 70)
+            print("ORDER DASHBOARD ".center(70))
+            print("=" * 70)
+
+            print("\n" + "-" * 25 + " OPTIONS " + "-" * 25)
+            print("1. Start a New Order")
+            print("2. View All Orders")
+            print("3. Update Existing Order (Add/Delete Items)")
+            print("4. Update Order Status")
+            print("5. Delete an Order")
+            print("6. View Menu ")
+            print("7. Back")
+
+            choice = input("Enter your choice (1-7): ")
+            choice = self.validator.validate_choice(choice, 1, 7)
+
+            if choice is None:
+                continue
+
+            if choice == 1:
+                print("\n--- Starting a New Order ---")
+                order_id = self.order.start_order()
+                print(f"Your Order ID is: {order_id}")
+                
+                while True:
+                    print("\nAdd Items to Order")
+                    self.order.add_item_to_order(order_id)
+
+                    while True:
+                        more = input("Would the customer like to add another item? (yes/no): ").lower()
+
+                        if more == "yes":
+                            break
+                        elif more == "no":
+                            print("\nOrder Completed\n")
+                            break
+                        else:
+                            print("Please enter 'yes' or 'no'")
+                    if more=="no":
+                        break
+
+            elif choice == 2:
+                print("\n--- Viewing All Orders ---")
+                self.order.view_all_orders()
+
+            elif choice == 3:
+                while True:
+                    print("\n--- Updating an Existing Order ---")
+                    order_id = input("Enter the Order ID you want to update: ")
+                    order_id = self.validator.validate_order_id(order_id)
+
+                    if order_id is not None:
+                       order_id = int(order_id)
+                       break
+                while True:
+                    print("\n" + "-" * 20 + " UPDATE OPTIONS " + "-" * 20)
+                    print("1. Add Item")
+                    print("2. Remove Item")
+                    print("3. Back")
+                    print("-" * 70)
+                    opt = input("Choose an option (1-3): ")
+                    opt = self.validator.validate_choice(opt, 1, 3)
+
+                    if opt is None:
+                        continue
+                    if opt == 1:
+                        self.order.add_item_to_order(order_id)
+                    elif opt == 2:
+                        self.order.delete_item_from_order(order_id)
+                    elif opt == 3:
+                        break
+                    else:
+                        print("Invalid choice, please try again.")
+
+            elif choice == 4:
+                while True:
+                    print("\n--- Updating Order Status ---")
+                
+                    order_id = input("Enter the Order ID to update status: ")
+                    order_id = self.validator.validate_order_id(order_id)
+
+                    if order_id is not None:
+                        order_id=int(order_id)
+                        break
+                self.order.update_order_status(order_id)   
+                    
+
+            elif choice == 5:
+                while True:
+                    print("\n--- Deleting an Order ---")
+                    order_id = input("Enter the Order ID you want to delete: ")
+                    order_id = self.validator.validate_order_id(order_id)
+
+                    if order_id is not None:
+                        order_id=int(order_id)
+                        break
+                while True:
+                    confirm = input("Are you sure? (yes/no): ").lower()
+                    if confirm == "yes":
+                        self.order.delete_order(order_id)
+                        break
+                    elif confirm == "no":
+                       print("Deletion cancelled")
+                       break
+
+                    else:
+                        print("Invalid input! Please enter 'yes' or 'no'")
+
+            elif choice==6:
+                view = ViewMenu()
+                view.view_menu()
+
+            elif choice == 7:
+                print("Exiting Order Dashboard.")
+                break
+
+            else:
+                print("Invalid choice, please enter a number from 1 to 6.")
---
+++ App/order/order_operation.py
@@ -0,0 +1,206 @@
+import json
+import uuid
+from menu.menu_operation import MenuOperations
+from validation.common_validation import CommonValidation
+class OrderOperations:
+
+    def read_orders(self):
+        try:
+            with open("App/database/order.json", "r") as file:
+                return json.load(file)
+        except:
+            return []
+
+    def write_orders(self, data):
+        with open("App/database/order.json", "w") as file:
+            json.dump(data, file, indent=4)
+
+    
+    def generate_order_id(self):
+        return int(str(uuid.uuid4().int)[:10])
+
+    
+    def start_order(self):
+        return self.generate_order_id()
+
+    
+    def add_item_to_order(self, order_id):
+        orders = self.read_orders()
+
+        
+        order = None
+        for o in orders:
+            if o["order_id"] == order_id:
+                order = o
+                break
+
+        
+        if order is None:
+            order = {
+                "order_id": order_id,
+                "items": [],
+                "total_bill": 0.0,
+                "status": "Pending"
+            }
+            orders.append(order)
+
+        menu= MenuOperations() 
+        validator=CommonValidation()
+        while True:
+            item_id = input("Enter item ID: ")
+            item_id = validator.validate_item_id(item_id)
+
+            if item_id is None:
+                continue
+            item = menu.get_item_by_id(item_id)
+
+            if item is None:
+                print("Item not found in menu. Try again.")
+                continue
+            
+            break
+
+        item_name = item["name"]
+        price = item["price"]
+
+        print(f"Selected Item Name: {item_name} - Price of Item: ₹{price}")
+        while True:
+            quantity = input("Enter quantity: ")
+            try:
+                quantity = int(quantity)
+
+                if quantity <= 0:
+                    print("Quantity must be greater than 0")
+                    continue
+
+                break
+            except:
+                print("Invalid input. Enter numbers only")
+        
+        total = quantity * price
+
+        
+        order["items"].append({
+            "item_id": item_id,
+            "item_name": item_name,
+            "quantity": quantity,
+            "price": price,
+            "total": total
+        })
+
+        
+        order["total_bill"] = sum(i["total"] for i in order["items"])
+
+        self.write_orders(orders)
+        print(f"Item added to Order ID: {order_id}. Current Total: {order['total_bill']}")
+
+    
+    def view_order(self, order_id):
+        orders = self.read_orders()
+        for order in orders:
+            if order["order_id"] == order_id:
+                print("\n" + "=" * 70)
+                print(f"ORDER DETAILS (ID: {order_id})".center(70))
+                print("=" * 70)
+
+                print(f"{'Item':25} {'Qty':5} {'Price':10} {'Total':10}")
+                print("-" * 70)
+
+                for item in order["items"]:
+                    print(f"{item['item_name']:25} {item['quantity']:5} ₹{item['price']:8} ₹{item['total']:8}")
+
+                print("-" * 70)
+                print(f"{'TOTAL BILL':45} ₹{order['total_bill']}")
+                print(f"Status: {order['status']}")
+                print("=" * 70)
+                return
+
+        print("Order not found")
+
+    
+    def view_all_orders(self):
+        orders = self.read_orders()
+
+        if not orders:
+            print("\nNo orders found")
+            return
+
+        for order in orders:
+            self.view_order(order["order_id"])
+
+    
+    def update_order_status(self, order_id):
+        orders = self.read_orders()
+        validator=CommonValidation()
+        for order in orders:
+            if order["order_id"] == order_id:
+                print("1. Pending\n2. Completed\n3. Cancelled")
+                
+                while True:
+                    choice = input("Enter choice: ")
+                    choice = validator.validate_choice(choice, 1, 3)
+
+                    if choice is not None:
+                       break
+                    
+                if choice == 1:
+                    order["status"] = "Pending"
+                elif choice == 2:
+                    order["status"] = "Completed"
+                elif choice == 3:
+                    order["status"] = "Cancelled"
+                else:
+                    print("Invalid choice")
+                    return
+                self.write_orders(orders)
+                print("Status Updated Successfully")
+                return
+        print("Order not found")
+
+    
+    def delete_order(self, order_id):
+        orders = self.read_orders()
+        new_orders = [o for o in orders if o["order_id"] != order_id]
+        if len(new_orders) != len(orders):
+            self.write_orders(new_orders)
+            print(f"Order ID {order_id} deleted")
+        else:
+            print("Order not found")
+    
+    def delete_item_from_order(self, order_id):
+        orders = self.read_orders()
+        order = next((o for o in orders if o["order_id"] == order_id), None)
+
+        if not order:
+            print("Order not found")
+            return
+
+        if not order["items"]:
+            print("No items to delete in this order")
+            return
+
+        print("Items in this order:")
+        print("-" * 60)
+        for idx, item in enumerate(order["items"], start=1):
+            print(f"{idx}. {item['item_name']} (ID: {item['item_id']}) - Qty: {item['quantity']} - Total: {item['total']}")
+        
+        while True:
+            try:
+                choice =input("Enter the item number which you want to delete: ")
+                choice=int(choice)
+                if choice<=0:
+                    print("Choice must be greater than 0")
+                    continue
+                if 1 <= choice <= len(order["items"]):
+                    removed = order["items"].pop(choice - 1)
+                    order["total_bill"] = sum(i["total"] for i in order["items"])
+                    self.write_orders(orders)
+                    print(f"\nRemoved {removed['item_name']}")
+                    print(f"New Total: ₹{order['total_bill']}")
+                    break
+                else:
+                    print("Invalid choice")
+
+            except:
+                print("Invalid input! Enter numbers only.")
+    
