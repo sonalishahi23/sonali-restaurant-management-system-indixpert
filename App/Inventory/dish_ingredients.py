8f9a9f9cd1b9e5202db342c893e0aac27b45ab96
sonalishahi23 <sonalirajput160@gmail.com>
2026-04-02 19:58:42 +0530

    Inventory

---
+++ App/Inventory/dish_ingredients.py
@@ -0,0 +1,56 @@
+dish_ingredients={
+    # Breakfast
+    "Aloo Paratha": ["Wheat Flour", "Potato", "Oil", "Salt"],
+    "Poha": ["Rice", "Onion", "Peas", "Oil", "Salt"],
+    "Upma": ["Suji", "Onion", "Carrot", "Oil", "Salt"],
+    "Idli Sambhar": ["Rice", "Suji", "Oil", "Salt"],
+    "Masala Dosa": ["Rice", "Urad Dal", "Potato", "Oil", "Salt"],
+
+    # Lunch
+    "Dal Tadka": ["Dal", "Oil", "Garlic", "Salt", "Turmeric", "Red Chilli Powder"],
+    "Rajma Chawal": ["Rajma", "Rice", "Onion", "Oil", "Salt", "Garlic"],
+    "Chole Chawal": ["Chickpeas", "Rice", "Onion", "Tomato", "Oil", "Salt"],
+    "Kadhi Chawal": ["Besan", "Curd", "Rice", "Oil", "Salt", "Turmeric"],
+    "Jeera Rice": ["Rice", "Jeera", "Oil", "Salt"],
+
+    # Dinner
+    "Paneer Butter Masala": ["Paneer", "Butter", "Cream", "Tomato", "Garlic", "Oil", "Salt"],
+    "Kadai Paneer": ["Paneer", "Capsicum", "Tomato", "Onion", "Oil", "Salt"],
+    "Dal Makhani": ["Dal", "Butter", "Cream", "Tomato", "Garlic", "Oil", "Salt"],
+    "Butter Naan": ["Wheat Flour", "Butter", "Oil", "Salt"],
+    "Tandoori Roti": ["Wheat Flour", "Oil", "Salt"],
+
+    # South Indian
+    "Plain Dosa": ["Rice", "Urad Dal", "Oil", "Salt"],
+    "Rava Dosa": ["Suji", "Rice", "Oil", "Salt"],
+    "Medu Vada": ["Urad Dal", "Oil", "Salt"],
+    "Uttapam": ["Rice", "Urad Dal", "Onion", "Oil", "Salt"],
+    "Appam": ["Rice", "Suji", "Sugar", "Oil"],
+
+    # Beverages
+    "Cold Coffee": ["Milk", "Coffee", "Sugar", "Ice Cream Base"],
+    "Hot Coffee": ["Milk", "Coffee", "Sugar"],
+    "Masala Tea": ["Milk", "Tea Leaves", "Sugar", "Ginger", "Cardamom"],
+    "Lassi": ["Curd", "Sugar", "Milk"],
+    "Mango Shake": ["Milk", "Mango Pulp", "Sugar"],
+
+    # Snacks
+    "Veg Burger": ["Burger Bun", "Tomato", "Lettuce", "Cheese", "Oil"],
+    "French Fries": ["Potato", "Oil", "Salt"],
+    "Cheese Balls": ["Cheese", "Bread Crumbs", "Oil"],
+    "Paneer Tikka": ["Paneer", "Capsicum", "Onion", "Oil", "Yogurt", "Spices"],
+    "Nachos": ["Corn", "Cheese", "Tomato", "Oil", "Salt"],
+
+    "Chocolate Cake": ["Flour", "Sugar", "Cocoa Powder", "Butter", "Eggs", "Baking Powder", "Vanilla Essence"],
+    "Brownie": ["Flour", "Sugar", "Cocoa Powder", "Butter", "Eggs", "Chocolate Syrup"],
+    "Ice Cream": ["Milk", "Sugar", "Cream", "Ice Cream Base", "Vanilla Essence"],
+    "Gulab Jamun": ["Milk Powder", "Flour", "Sugar", "Oil", "Cardamom"],
+    "Black Forest Cake": ["Flour", "Sugar", "Cocoa Powder", "Butter", "Eggs", "Cherry", "Chocolate Syrup", "Cream"],
+
+    # Sandwiches
+    "Veg Sandwich": ["Sandwich Bread", "Onion", "Tomato", "Capsicum", "Cheese", "Mayonnaise"],
+    "Grilled Sandwich": ["Sandwich Bread", "Onion", "Tomato", "Capsicum", "Cheese", "Butter"],
+    "Cheese Sandwich": ["Sandwich Bread", "Cheese", "Butter"],
+    "Paneer Sandwich": ["Sandwich Bread", "Paneer", "Onion", "Tomato", "Capsicum", "Cheese", "Butter"],
+    "Club Sandwich": ["Sandwich Bread", "Paneer", "Tomato", "Lettuce", "Cheese", "Mayonnaise", "Butter"]
+}
